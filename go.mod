e0a892fb26735f5c75e188d917ec2ad96e5c17b2
bytesbanana <saharat.paynok@gmail.com>
2024-05-19 14:48:02 +0700

    add create user unit test

--- go.mod
+++ go.mod
@@ -3,17 +3,21 @@ module bytesbanana/realworld-go-echo
 go 1.22.1
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.2
 	github.com/jmoiron/sqlx v1.4.0
 	github.com/labstack/echo/v4 v4.12.0
 	github.com/lib/pq v1.10.9
+	github.com/stretchr/testify v1.8.4
 	go.uber.org/zap v1.27.0
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
@@ -22,4 +26,5 @@ require (
 	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ src/internal/adapter/db/user_test.go
@@ -0,0 +1,35 @@
+package db
+
+import (
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/jmoiron/sqlx"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateUser(t *testing.T) {
+
+	t.Run("given user should create user", func(t *testing.T) {
+
+		mockDB, mock, err := sqlmock.New()
+		assert := assert.New(t)
+		assert.NoError(err, "an error '%s' was not expected when opening a stub database connection", err)
+
+		rows := sqlmock.NewRows([]string{"id", "email", "username", "hashed_password"}).
+			AddRow(1, "testuser@test.com", "username", "password")
+
+		mock.ExpectQuery("INSERT INTO users").WithArgs("testuser@test.com", "username", "password").WillReturnRows(rows)
+
+		sqlxDB := sqlx.NewDb(mockDB, "sqlmock")
+		ur := NewUserRepository(sqlxDB)
+
+		u, err := ur.CreateUser("testuser@test.com", "username", "password")
+		assert.NoError(err)
+		assert.NotNil(u, "user should not be null")
+		assert.Equal(u.ID, 1)
+		assert.Equal(u.Email, "testuser@test.com")
+		assert.Equal(u.Username, "username")
+		assert.NotEmpty(u.HashedPassword)
+	})
+}
